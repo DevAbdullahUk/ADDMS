bd0a4bfcbd767d544cd5bfdf500e466e8e5fd0eb
Khalil Mayyasi <khello1996@gmail.com>
2018-04-22 17:47:57 +0300

    Retrieving data from firestore for account home page

--- app/src/main/java/com/capstone/ako/addms/accountHomePage.java
+++ app/src/main/java/com/capstone/ako/addms/accountHomePage.java
@@ -7,21 +7,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class accountHomePage extends AppCompatActivity {
     // elements on the screen
-    TextView userName_tv;
+    TextView userName_tv, alerts, avgSpeed, tripTime, distance;
+
+    //Firebase
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_home_page);
         userName_tv = findViewById(R.id.userName);
+        alerts = findViewById(R.id.txt_Alerts);
+        avgSpeed = findViewById(R.id.txt_AvgSpeed);
+        tripTime = findViewById(R.id.txt_Time);
+        distance = findViewById(R.id.txt_Distance);
         // Save the login data
         final SharedPreferences login_Data = PreferenceManager.getDefaultSharedPreferences(this);
         userName_tv.setText( login_Data.getString("userName","0"));
 
+        // Firebase Firestore object
+        db = FirebaseFirestore.getInstance();
+
+
+        db.collection("MobToWeb").document(login_Data.getString("userName","0")).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                try {
+
+                    alerts.setText(documentSnapshot.getData().get("Alerts").toString());
+                    avgSpeed.setText(documentSnapshot.getData().get("AverageSpeed").toString()+" \nkm/h");
+                    distance.setText(documentSnapshot.getData().get("Distance").toString()+" \nKm");
+                    tripTime.setText(documentSnapshot.getData().get("Time").toString()+ " \nmin");
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                    Toast.makeText(accountHomePage.this, "No available trip history ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
     public void account(View view) {
         startActivity(new Intent(accountHomePage.this, accountScreen.class));
